7882820990710a8782de6ce1e734441be4599d72
Chandan <creativemind.kumar5@gmail.com>
2024-06-21 23:55:03 +0530

    add

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-# build_files.sh
-pip install -r requirements.txt
-pip freeze > requirements.txt
-
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
--- inventory_management_system/settings.py
+++ inventory_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t!x@!u4lg!^l45e8*5_mz#j0f38^u!rhhq=_8xotvb4et9s$!4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
